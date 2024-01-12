92db6fd816781a8b739c65faddd9b6f25f9b45bb
Adem Hodzic <athos.blade@gmail.com>
2024-01-12 14:15:37 +0100

    move everything into SingleLink compoennt

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,13 @@
 <script setup>
-import { ref } from 'vue'
 import SingleLink from './components/SingleLink.vue';
 
-const link = ref('')
 </script>
 
 <template>
-  <div class="container">
-    <SingleLink :link="link" />
-    <input type="text" v-model="link" placeholder="Paste a link to turn to QR Code">
-    <a :disabled="!link" :class="{ 'disabled': link === '', 'downloadButton': true }" download="qrcode.png"
-      :href="qrCodeImage">Download QR code as png</a>
-  </div>
+  <SingleLink />
+
 </template>
 
 <style lang="scss" scoped>
-:root {
-  background: var(--background);
-  color: var(--text);
-  font: 1rem "PT Sans", sans-serif;
-}
 
-input {
-  width: 360px;
-  background: #fff;
-  color: var(--input-text-color);
-  font: inherit;
-  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, .1);
-  border: 0;
-  outline: 0;
-  padding: 22px 18px;
-}
-
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  gap: 3rem;
-}
-
-.disabled {
-  pointer-events: none;
-  opacity: .5;
-}
-
-.downloadButton {
-  padding: 1rem;
-  border-radius: 0.5rem;
-  background-color: var(--button-bg-color);
-  color: white;
-  text-decoration: none;
-}
 </style>
--- src/components/SingleLink.vue
+++ src/components/SingleLink.vue
@@ -1,8 +1,15 @@
 <template>
+  <div class="container">
     <div>
         <p v-if="loading">Loading...</p>
-        <img v-if="props.link && !loading" :src="qrCodeImage" :alt="props.link">
+        <img v-if="link && !loading" :src="qrCodeImage" :alt="link">
     </div>
+    <input type="text" v-model="link" placeholder="Paste a link to turn to QR Code">
+    <a :disabled="!link" :class="{ 'disabled': link === '', 'downloadButton': true }" download="qrcode.png"
+      :href="qrCodeImage">Download QR code as png</a>
+  </div>
+
+
 </template>
 
 <script setup>
@@ -10,12 +17,11 @@
 import { ref, watch } from 'vue'
 import makeQr from "../qrcode"
 
-const props = defineProps(['link'])
-
 const loading = ref(false)
 const qrCodeImage = ref('')
+const link = ref('')
 
-watch(() => props.link, async (newLink) => {
+watch(link, async (newLink) => {
     loading.value = true
     try {
         const qr = await makeQr(newLink)
@@ -31,6 +37,38 @@ watch(() => props.link, async (newLink) => {
 
 <style scoped>
 
+input {
+  width: 360px;
+  background: #fff;
+  color: var(--input-text-color);
+  font: inherit;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, .1);
+  border: 0;
+  outline: 0;
+  padding: 22px 18px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  gap: 3rem;
+}
+
+.disabled {
+  pointer-events: none;
+  opacity: .5;
+}
+
+.downloadButton {
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background-color: var(--button-bg-color);
+  color: white;
+  text-decoration: none;
+}
 
 
 img {
